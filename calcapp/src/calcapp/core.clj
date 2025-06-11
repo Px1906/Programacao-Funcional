5fa278e7c391b13e74556b6a7476acd4af56b571
Sérgio de Paiva <115050687+Px1906@users.noreply.github.com>
2025-06-11 17:48:05 -0300

    Inicio dos tratamentos de erro

--- calcapp/src/calcapp/core.clj
+++ calcapp/src/calcapp/core.clj
@@ -3,26 +3,52 @@
   (:require [clj-http.client :as http-client]
             [cheshire.core :as json]))
 
+(defn lerInt []
+  (loop []
+    (let [input (read-line)]
+      (if (re-matches #"-?\d+" input)
+        (Integer/parseInt input)
+        (do
+          (println "Por favor digite um numero interio")
+          (recur))))))
+
+(defn lerIdade []
+  (loop []
+    (let [input (read-line)]
+      (if (and (re-matches #"\d+" input)
+               (let [idade (Integer/parseInt input)]
+                 (and (>= idade 8) (<= idade 125))))
+        (Integer/parseInt input)
+        (do
+          (println "Por favor, digite uma idade entre 8 e 125 anos.")
+          (recur))))))
+
+(defn ler1ou2 []
+  (loop []
+    (let [input (read-line)]
+      (if (or (= input "1") (= input "2"))
+        (Integer/parseInt input)
+        (do
+          (println "Por favor, digite 1 ou 2")
+          (recur))))))
+
 (defn novoCadastro []
-  (println "NOVO CADASTRO:")
-  (println "Digite o número com base no seu sexo: \n1. Masculino \n2. Feminino")
-  (let [sexo (read)]
+  (println "\nNOVO CADASTRO:")
+  (println "Digite o numero com base no seu sexo: \n1. Masculino \n2. Feminino")
+  (let [sexo (ler1ou2)]
     (println "Digite sua idade:")
-    (let [idade (read)]
+    (let [idade (lerIdade)]
       (println "Digite seu peso:")
-      (let [peso (read)
+      (let [peso (lerInt)
             cadastro {:sexo sexo :idade idade :peso peso}]
-        (println "\nDados cadastrados:" cadastro)
-        (println "Enviando dados para o servidor...")
-        (let [resposta (http-client/post "http://localhost:3001/cadastro"
-                                         {:headers {"Content-Type" "application/json"}
-                                          :body (json/generate-string cadastro)})]
-          (println "Resposta do servidor:" (:status resposta))
-          (println "Corpo da resposta:" (:body resposta)))
+        (println "Dados cadastrados com sucesso")
+        (http-client/post "http://localhost:3001/cadastro"
+                          {:headers {"Content-Type" "application/json"}
+                           :body (json/generate-string cadastro)})
         cadastro))))
 
 (defn menuCadastro []
-  (println "OPÇÕES DE CADASTRO:\n1. Novo cadastro\n2. Consulta")
+  (println "\nOPCOES DE CADASTRO:\n1. Novo cadastro\n2. Consulta")
   (case (read)
     1 (novoCadastro)
     2 (let [resposta (http-client/get "http://localhost:3001/cadastro"
@@ -30,24 +56,22 @@
             corpo-json (:body resposta)
             mapa (json/parse-string corpo-json true)
             {:keys [sexo idade peso]} mapa]
-        (println "\n--- Dados recebidos do servidor ---")
-        (println "Map Clojure:" mapa)
-        (println "Sexo:" (if (= sexo 1) "Masculino" "Feminino"))
-        (println "Idade:" idade)
-        (println "Peso:" peso))
-    (println "Opção inválida")))
+        (println "\nSexo:" (if (= sexo 1) "Masculino" "Feminino"))
+        (println "Idade:" idade "anos")
+        (println "Peso:" peso "Kg\n"))
+    (println "Opcao invalida")))
 
 (defn menuGanhoCalorico []
-  (println "\nREGISTRO DE GANHO CALÓRICO (ALIMENTO):")
+  (println "\nREGISTRO DE GANHO CALORICO (ALIMENTO):")
   (println "Digite o nome do alimento:")
   (let [alimento (read)]
     (println "Digite a quantidade (em gramas):")
-    (let [quantidade (read)]
+    (let [quantidade (lerInt)]
       (println "Digite a data (formato: dd-mm-yyyy):")
       (let [data (read)
             registro {:alimento alimento :quantidade quantidade :data data}]
         (println "\nEnviando dados do alimento para o servidor...")
-        (let [resposta (http-client/post "http://localhost:3001/ganho";;Fiquei na duvida se coloca cadastro tbm
+        (let [resposta (http-client/post "http://localhost:3001/ganho"
                                          {:headers {"Content-Type" "application/json"}
                                           :body (json/generate-string registro)})]
           (println "Resposta do servidor:" (:status resposta))
@@ -55,17 +79,17 @@
         registro))))
 
 
-        (defn menuPerdaCalorica []
-  (println "\nREGISTRO DE PERDA CALÓRICA (ATIVIDADE FÍSICA):")
-  (println "Digite o nome da atividade física:")
-  (let [atividade (read)]
-    (println "Digite a duração (em minutos):")
+(defn menuPerdaCalorica []
+  (println "\nREGISTRO DE PERDA CALORICA (ATIVIDADE FISICA):")
+  (println "Digite o nome da atividade fisica:")
+  (let [atividade (lerInt)]
+    (println "Digite a duracao (em minutos):")
     (let [duracao (read)]
       (println "Digite a data (formato: dd-mm-yyyy):")
       (let [data (read)
             registro {:atividade atividade :duracao duracao :data data}]
         (println "\nEnviando dados da atividade para o servidor...")
-        (let [resposta (http-client/post "http://localhost:3001/perda";;Fiquei na duvida se coloca cadastro tbm
+        (let [resposta (http-client/post "http://localhost:3001/perda"
                                          {:headers {"Content-Type" "application/json"}
                                           :body (json/generate-string registro)})]
           (println "Resposta do servidor:" (:status resposta))
@@ -74,14 +98,13 @@
 
 
 (defn menu []
-  (println "MENU:\n1. Cadastro\n2. Ganho calórico\n3. Perda calórica")
+  (println "\nMENU:\n1. Cadastro\n2. Ganho calorico\n3. Perda calorica")
   (case (read)
     1 (menuCadastro)
     2 (menuGanhoCalorico)
     3 (menuPerdaCalorica)
-    (println "Opção inválida"))
-   (recur) ;; se quiser loop contínuo
-  )
+    (println "Opcao invalida"))
+  (recur))
 
 (defn -main
   "Ponto de entrada do programa."
